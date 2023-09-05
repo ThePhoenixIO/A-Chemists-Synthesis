09e4f3ec0cc7129317f6b7cb4eb5155b328f2222
ThePhoenixIO <phoenixgate20@gmail.com>
2023-09-04 23:31:06 -0700

    started class decloations of next reaction classes

--- Reagent.hpp
+++ Reagent.hpp
@@ -1,108 +1,16 @@
 #pragma once
 #include "Compound.hpp"
+#include "Reactant.hpp"
 
-class reagent : protected compound
+class reagent : public compound, reactant
 {
-protected:
-	double mv;
-	double mol;
-	double mmol;
-	float eq;
-
-	bool isReagent;
-	bool isProduct;
-	
 public:
 	reagent();
-	reagent(double reagentMV, double reagentMOL, float reagentEQ, const char* flag = "reagent");
 
-	int setMV(double reagentMV);
-	int setMOL(double reagentMOL);
-	int setEQ(float reagentEQ);
+private:
 
-	int setFlag(const char* flag = "reagent");
-
-	int setReagent(double reagentMV, double reagentMOL = -1, float reagentEQ = -1, const char* flag = "reagent");
 };
 
 reagent::reagent()
 {
-	mv = -1;
-	mol = -1;
-	mmol = mol / 1000;
-	eq = -1;
-	
-	isReagent = false;
-	isProduct = false;
-}
-
-reagent::reagent(double reagentMV, double reagentMOL, float reagentEQ, const char* flag = "reagent")
-{
-	mv = reagentMV;
-	mol = reagentMOL;
-	mmol = mol / 1000;
-	eq = reagentEQ;
-
-	setFlag(flag);
-}
-
-int reagent::setMV(double reagentMV)
-{
-	mv = reagentMV;
-	
-	return 0;
-}
-
-int reagent::setMOL(double reagentMOL)
-{
-	mol = reagentMOL;
-	mmol = mol / 1000;
-	
-	return 0;
-}
-
-int reagent::setEQ(float reagentEQ)
-{
-	eq = reagentEQ;
-
-	return 0;
-}
-
-int reagent::setFlag(const char* flag)
-{
-	if (flag == "reagent")
-	{
-		isReagent = true;
-
-		return 0;
-	}
-	if (flag == "product")
-	{
-		isProduct = true;
-		
-		return 0;
-	}
-	else
-	{
-		return 1;
-	}
-}
-
-int reagent::setReagent(double reagentMV, double reagentMOL, float reagentEQ, const char* flag = "reagent")
-{
-	mv = reagentMV;
-	mol = reagentMOL;
-	mmol = mol / 1000;
-	eq = reagentEQ;
-
-	int flagVal = setFlag(flag);
-
-	if (flagVal == 0)
-	{
-		return 0;
-	}
-	else
-	{
-		return flagVal;
-	}
 }
---
+++ StartingMaterial.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include "Compound.hpp"
+#include "Reactant.hpp"
+
+class startingMateriale : public compound, reactant
+{
+public:
+	startingMateriale();
+
+private:
+
+};
+
+startingMateriale::startingMateriale()
+{
+}
