13978947c56cdf6874d1f363dba3354ba02fd19e
ThePhoenixIO <phoenixgate20@gmail.com>
2023-10-25 08:29:43 -0700

    edited converter code

--- converter.hpp
+++ converter.hpp
@@ -2,11 +2,13 @@
 #include"startingOrProduct.hpp"
 #include "Reagent.hpp"
 
-reagent* convertReagent(startingOrProduct* convertee) {
+reagent* convertReagent(startingOrProduct* convertee, startingOrProduct* sm) {
 	reagent* converted = new reagent(convertee->baseCompound);
 	converted->setMol(convertee->getMol());
 	converted->setMV(convertee->getMV(), convertee->getMVU());
 	converted->setEquivalents(1);
+
+	converted->calculateReagent(sm);
 
 	return converted;
 }
