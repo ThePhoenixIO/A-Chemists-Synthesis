20189540841ae0605084641d0b6d706b4e450eb1
ThePhoenixIO <phoenixgate20@gmail.com>
2024-02-08 12:54:44 -0800

    added code comments and cleaded up code

--- converter.hpp
+++ converter.hpp
@@ -2,13 +2,20 @@
 #include"startingOrProduct.hpp"
 #include "Reagent.hpp"
 
-reagent* convertReagent(startingOrProduct* convertee, startingOrProduct* sm) {
+/*
+* Function: convertReagent
+* Arguments: startingOrProduct* convertee, startingOrProduct* startingMaterial
+* Warnings: None
+* Description: Converts a startingOrProduct to a reagent
+* Returns: reagent*
+*/
+reagent* convertReagent(startingOrProduct* convertee, startingOrProduct* startingMaterial) {
 	reagent* converted = new reagent(convertee->baseCompound);
 	converted->setMol(convertee->getMol());
 	converted->setMV(convertee->getMV(), convertee->getMVU());
 	converted->setEquivalents(1);
 
-	converted->calculateReagent(sm);
+	converted->calculateReagent(startingMaterial);
 
 	return converted;
 }
