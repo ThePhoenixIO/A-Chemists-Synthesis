19968012b7c13849439e917f6e63223566b35514
ThePhoenixIO <phoenixgate20@gmail.com>
2024-02-08 12:54:09 -0800

    Added comments, and cleaned up code

--- Compound.hpp
+++ Compound.hpp
@@ -1,33 +1,47 @@
 #pragma once
 #include<iostream>
 #include<fstream>
+#include<unordered_map>
 
 #include"tinyxml2/tinyxml2.h"
 
 #ifndef COMPOUND
 #define COMPOUND
+// Compiler toggle for setters
 #define SETTERS false
 
+/*
+* Compound Class
+* Independet from reactions
+* Used to store common information about compounds
+* Accessable through 2 maps
+* 1. Common Compound Map [commonCompoundMap] - Only used for common compounds loaded at startup from file	
+* 2. Synthesis Compound Map [compoundMap]
+*/
 class compound
 {
 protected:
+	// Compound Attributes
     const char* name;
     const char* formula;
     double molecularWeight;
 	
 public:
+	// Constructors
     compound();
 	compound(const char* compoundName, const char* compoundFormula, double compoundMW);
     
-    // setter methods
+    // Setter methods (if needed)
 	#if SETTERS
 	int setName(const char* compoundName);
 	int setFormula(const char* compoundFormula);
 	int setMW(double compoundMW);
 	#endif // SETTERS
 	
+	// Method for setting all attributes at once
     int setCompound(const char* compoundName, const char* compoundFormula, double compoundMW);
-
+	
+	// Method for setting all attributes at once from user input
 	int userSetCompound();
 
 	// getter methods
@@ -35,11 +49,19 @@ public:
 	const char* getFormula();
 	double getMW();
 
+	// XML Method
 	int saveToXML(tinyxml2::XMLDocument* doc, tinyxml2::XMLElement* baseNode);
 };
 
 // Constructors
 
+/*
+* Constructor: compound - Default
+* Arguments: none
+* Warnings: Hardcoded values [UNDEFINED and -1.0]
+* Descriptiom: Default constructor for compound class
+* Returns: none
+*/
 compound::compound()
 {
     this->name = "UNDEFINED";
@@ -47,6 +69,13 @@ compound::compound()
 	this->molecularWeight = -1.0;
 }
 
+/*
+* Constructor: compound - Primary
+* Arguments: const char* compoundName, const char* compoundFormula, double compoundMW
+* Warnings: none
+* Descriptiom: constructor for compound class
+* Returns: none
+*/
 compound::compound(const char* compoundName, const char* compoundFormula, double compoundMW)
 {
 	this->name = compoundName;
@@ -54,6 +83,10 @@ compound::compound(const char* compoundName, const char* compoundFormula, double
 	this->molecularWeight = compoundMW;
 }
 
+/*
+* Setter Methods
+* WARNING CHECK TOGGLE IF NEEDED
+*/
 #if SETTERS
 //Setter Methods
 //Methods for setting individual attributes
@@ -79,6 +112,13 @@ int compound::setMW(double compoundMW)
 }
 #endif // SETTERS
 
+/*
+* Method: setCompound
+* Arguments: const char* compoundName, const char* compoundFormula, double compoundMW
+* Warnings: comoundFormula default value is "UNDEFINED", compoundMW default value is -1
+* Descriptiom: method for setting all attributes at once
+* Returns: 0 on success
+*/
 int compound::setCompound(const char* compoundName, const char* compoundFormula = "UNDEFINED", double compoundMW = -1)
 {
 	this->name = compoundName;
@@ -88,12 +128,29 @@ int compound::setCompound(const char* compoundName, const char* compoundFormula 
 	return 0;
 }
 
+/*
+* Method: userSetCompound
+* Arguments: none
+* Warnings: Buffer overflow possible
+* Descriptiom: method for setting all attributes at once from user input
+* Returns: 0 on success
+* 
+* Todo: add buffer overflow protection
+* Todo: add compile time allocation of buffer
+* Todo: add return codes
+*/
 int compound::userSetCompound()
 {
+	// creates variables for user input
+	// Warning: buffer overflow possible
+	// Buffer limit set to 100
+	// TODO: add compile time allocation of buffer
+	// TODO: add buffer overflow protection
 	char compoundName[100];
 	char compoundFormula[100];
 	double compoundMolarMass;
 
+	// Prompts the user to set the attributes of the compound
 	std::cout << "Enter the name of the compound: ";
 	std::cin >> compoundName;
 
@@ -110,40 +167,141 @@ int compound::userSetCompound()
 
 // Getter Methods
 
+/*
+* Method: getName
+* Arguments: none
+* Warnings: none
+* Descriptiom: method for getting compound name
+* Returns: const char* name
+*/
 const char* compound::getName()
 {
 	return this->name;
 }
 
+/*
+* Method: getFormula
+* Arguments: none
+* Warnings: none
+* Descriptiom: method for getting compound formula
+* Returns: const char* formula
+*/
 const char* compound::getFormula()
 {
 	return this->formula;
 }
 
+/*
+* Method: getMW
+* Arguments: none
+* Warnings: none
+* Descriptiom: method for getting compound molecular weight
+* Returns: double molecularWeight\
+*/
 double compound::getMW()
 {
 	return this->molecularWeight;
 }
 
-inline int compound::saveToXML(tinyxml2::XMLDocument* doc, tinyxml2::XMLElement* baseNode)
+/*
+* Method: saveToXML
+* Arguments: tinyxml2::XMLDocument* doc, tinyxml2::XMLElement* baseNode
+* Warnings: none
+* Descriptiom: method for saving compound to XML map given a base node
+* Returns: 0 on success
+* Todo: add return codes
+*/
+int compound::saveToXML(tinyxml2::XMLDocument* doc, tinyxml2::XMLElement* baseNode)
 {
+	// Create compound node
 	tinyxml2::XMLElement* xmlCompound = doc->NewElement("compound");
 
-	tinyxml2::XMLElement* name = commonCompoundXML.NewElement("name");
+	// Create, set, and insert name element
+	tinyxml2::XMLElement* name = doc->NewElement("name");
 	name->SetAttribute("type", "string");
 	name->SetText(this->getName());
 	xmlCompound->InsertFirstChild(name);
 
-	tinyxml2::XMLElement* formula = commonCompoundXML.NewElement("formula");
+	// Create, set, and insert formula element
+	tinyxml2::XMLElement* formula = doc->NewElement("formula");
 	formula->SetAttribute("type", "string");
 	formula->SetText(this->getFormula());
 	xmlCompound->InsertEndChild(formula);
 
-	tinyxml2::XMLElement* molecularWeight = commonCompoundXML.NewElement("molecular_weight");
+	// Create, set, and insert molecular weight element
+	tinyxml2::XMLElement* molecularWeight = doc->NewElement("molecular_weight");
 	molecularWeight->SetAttribute("type", "double");
 	molecularWeight->SetText(this->getMW());
 	xmlCompound->InsertEndChild(molecularWeight);
 
+	// Insert compound node into base node
 	baseNode->InsertEndChild(xmlCompound);
+	
+	return 0;
+}
+
+/*
+* Function: writeCompoundXML
+* Arguments: const char* path, std::unordered_map<const char*, compound*>* map
+* Warnings: map is a pointer!!!
+* Descriptiom: function for writing compound map to XML file
+* Returns: 0 on success
+* Todo: add return codes
+* Todo: add error handling
+* Todo: check and make path
+*/
+int writeCompoundXML(const char* path, std::unordered_map<const char*, compound*>* map) {
+	// Create XML document
+	tinyxml2::XMLDocument compoundMap;
+
+	// Create and insert declaration
+	compoundMap.InsertFirstChild(compoundMap.NewDeclaration("xml version=\"1.0\" encoding=\"UTF-8\""));
+	
+	// Create and insert base node
+	tinyxml2::XMLElement* xmlCompoundMap = compoundMap.NewElement("compound_map");
+
+	//Take this code, compound code, and common compound code and combine to single compound method to return
+	// element node pre constructed
+	
+	/* TODO List
+	* take this code combine with similar code elseware and make single function
+	*/
+
+	// initialize iterator [it]
+	auto it = map->begin();
+	tinyxml2::XMLElement* name = nullptr;
+	tinyxml2::XMLElement* formula = nullptr;
+	tinyxml2::XMLElement* molecularWeight = nullptr;
+	for (int n = 0; n < map->size(); n++, it++)
+	{
+		compound* comp = it->second;
+		tinyxml2::XMLNode* xmlCompound = compoundMap.NewElement("compound");
+
+		// Create, set, and insert name element
+		name = compoundMap.NewElement("name");
+		name->SetAttribute("type", "string");
+		name->SetText(comp->getName());
+		xmlCompound->InsertFirstChild(name);
+
+		// Create, set, and insert formula element
+		formula = compoundMap.NewElement("formula");
+		formula->SetAttribute("type", "string");
+		formula->SetText(comp->getFormula());
+		xmlCompound->InsertEndChild(formula);
+
+		// Create, set, and insert molecular weight element
+		molecularWeight = compoundMap.NewElement("molecular_weight");
+		molecularWeight->SetAttribute("type", "double");
+		molecularWeight->SetText(comp->getMW());
+		xmlCompound->InsertEndChild(molecularWeight);
+
+		xmlCompoundMap->InsertEndChild(xmlCompound);
+	}
+
+	// Insert base node into document
+	compoundMap.InsertEndChild(xmlCompoundMap);
+	compoundMap.SaveFile(path);
+
+	return 0;
 }
 #endif // !COMPOUND
