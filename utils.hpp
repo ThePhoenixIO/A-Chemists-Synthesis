4df358057ef483ffd8aa77ccad272cd4505f6642
ThePhoenixIO <phoenixgate20@gmail.com>
2024-02-16 12:04:33 -0800

    Edited Concentration function to allow for any conditioin

--- utils.hpp
+++ utils.hpp
@@ -12,29 +12,33 @@ struct concentrationTable {
 * Description: Calculates the missing variable in a concentration table
 * Returns: The calculated value
 */
-float concentrationCalc(concentrationTable calcTable, char calcVar) {
+float concentrationCalc(concentrationTable calcTable, char calcVar)
+{
     switch (calcVar)
     {
-        case 'M':
-            if (calcTable.M2 == -1) {
-                return (calcTable.M1 * calcTable.V1) / calcTable.V2;
-            }
-            else if (calcTable.M1 == -1){
-                return (calcTable.M2 * calcTable.V2) / calcTable.V1;
-            }
-            else {
-                return -1;
-            }
-        case 'V':
-            if (calcTable.V2 > 0 ) {
-                return (calcTable.M1 * calcTable.V1) / calcTable.M2;
-            }
-            else if (calcTable.V1 > 0){
-                return (calcTable.M2 * calcTable.V2) / calcTable.M1;
-            }
-            else {
-                return -1;
-            }
-        default:
+    case 'M':
+        if (calcTable.M2 == -1) {
+            return (calcTable.M1 * calcTable.V1) / calcTable.V2;
+        }
+        else if (calcTable.M1 == -1) {
+            return (calcTable.M2 * calcTable.V2) / calcTable.V1;
+        }
+        else {
             return -1;
+        }
+
+    case 'V':
+        if (calcTable.V2 > 0) {
+            return (calcTable.M1 * calcTable.V1) / calcTable.M2;
+        }
+        else if (calcTable.V1 > 0) {
+            return (calcTable.M2 * calcTable.V2) / calcTable.M1;
+        }
+        else {
+            return -1;
+        }
+
+    default:
+        return -1;
+    }
 }
