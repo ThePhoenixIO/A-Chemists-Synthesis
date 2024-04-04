dc3838eb08c25331d0e1458faa7e7ae0d47d510a
ThePhoenixIO <phoenixgate20@gmail.com>
2024-04-04 10:05:21 -0700

    added base file with all includes for the project

---
+++ base.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <windows.h>
+#include <commdlg.h>
+#include <shlobj.h>
+#include <direct.h>
+#include <iostream>
+#include <fstream>
+#include <unordered_map>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+#include "tinyxml2/tinyxml2.h"
+#include "tabulate/tabulate.hpp"
